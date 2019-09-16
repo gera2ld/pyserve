aed4e5da8dc3726f09cf4e119911acbf5e19b2b7
Gerald <gera2ld@163.com>
2019-09-17 00:25:57 +0800

    feat: export print_urls method

--- gera2ld/pyserve/__init__.py
+++ gera2ld/pyserve/__init__.py
@@ -2,6 +2,17 @@ import os
 import socket
 import asyncio
 
+class UrlItem:
+    TYPE_LOCAL = 'local', 'Local:'
+    TYPE_REMOTE = 'remote', 'Remote:'
+
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
+
+    def to_str(self):
+        return f'{self.type[1]}\t{self.data}'
+
 def get_host_ip():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,48 +23,68 @@ def get_host_ip():
         s.close()
     return ip
 
-def get_hosts(server, scheme):
-    for sock in server.sockets:
-        hostname = sock.getsockname()
-        if isinstance(hostname, str):
+def get_url_pairs(hosts, scheme):
+    for host in hosts:
+        if isinstance(host, str):
             # unix server
-            yield hostname, None
+            yield host, None
         else:
-            host, port = hostname[:2]
-            def add_port(host):
-                if host is None: return
-                return f'{scheme}//{host}:{port}'
-            if host == '0.0.0.0':
+            hostname, port = host[:2]
+            def add_port(hostname):
+                if hostname is None: return
+                return f'{scheme}//{hostname}:{port}'
+            if hostname == '0.0.0.0':
                 yield add_port('localhost'), add_port(get_host_ip())
-            elif host == '::':
+            elif hostname == '::':
                 yield add_port('[::1]'), None
             else:
-                yield None, add_port(host)
+                yield None, add_port(hostname)
 
-def wake_up(loop = None):
+def get_url_items(hosts, scheme='http:'):
+    for local, remote in get_url_pairs(hosts, scheme):
+        items = []
+        if local is not None:
+            items.append(UrlItem(type=UrlItem.TYPE_LOCAL, data=local))
+        if remote is not None:
+            items.append(UrlItem(type=UrlItem.TYPE_REMOTE, data=remote))
+        yield items
+
+def get_asyncio_server_hosts(server, scheme):
+    assert isinstance(server, asyncio.base_events.Server)
+    yield from get_url_items([sock.getsockname() for sock in server.sockets], scheme)
+
+def wake_up(loop=None):
     if os.name == 'nt':
         if loop is None: loop = asyncio.get_event_loop()
         def wake_up_later():
             loop.call_later(.1, wake_up_later)
         wake_up_later()
 
-def serve_forever(servers, loop=None, scheme='http:'):
+def run_forever(loop=None):
     if loop is None: loop = asyncio.get_event_loop()
-    print('====================')
-    first = True
-    if isinstance(servers, asyncio.base_events.Server):
-        servers = [servers]
-    for server in servers:
-        hosts = get_hosts(server, scheme)
-        for local, remote in hosts:
-            if first:
-                first = False
-            else:
-                print('---')
-            if local is not None:
-                print('Local:', local, sep='\t')
-            if remote is not None:
-                print('Remote:', remote, sep='\t')
-    print('====================')
     wake_up(loop)
     loop.run_forever()
+
+def print_urls(hosts):
+    print('====================')
+    first_host = True
+    for groups in hosts:
+        if first_host:
+            first_host = False
+        else:
+            print('***')
+        first_group = True
+        for group in groups:
+            if first_group:
+                first_group = False
+            else:
+                print('---')
+            for item in group:
+                print(item.to_str())
+    print('====================')
+
+def serve_forever(servers, loop=None, scheme='http:'):
+    if not isinstance(servers, (list, tuple)):
+        servers = [servers]
+    print_urls(get_asyncio_server_hosts(server, scheme) for server in servers)
+    run_forever(loop)
